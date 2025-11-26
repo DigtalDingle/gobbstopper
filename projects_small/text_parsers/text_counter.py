9b1d4a1bb4ae85e99f542d2ef90e1fdc2de9a74e
Joshua Berry <jberry8404@gmail.com>
2025-11-25 20:31:24 -0500

    Create text_counter.py

---
+++ projects_small/text_parsers/text_counter.py
@@ -0,0 +1,8 @@
+# Simple text counter
+text = "Gobbstopper is the project that never ends."
+
+word_count = len(text.split())
+char_count = len(text)
+
+print("Words:", word_count)
+print("Characters:", char_count)
